144465e51348156c632980bfe4cb286b197ce45e
Gabriel Alejandro Gomez <gabriel70@gmail.com>
2021-05-05 20:45:55 -0300

    with lint

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,11 +9,8 @@ import {
   Delete,
   HttpStatus,
   HttpCode,
-  Res,
-  // ParseIntPipe,
 } from '@nestjs/common';
 
-import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
